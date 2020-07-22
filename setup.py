48b0a34a72f3b2ca1240eb81b8371ff7d5ee52a2
Alex Waite <alex@waite.eu>
2020-07-22 10:54:22 +0200

    drop < python 3.6
    Rich supports only 3.6.1 and later.

--- setup.py
+++ setup.py
@@ -28,13 +28,12 @@ setuptools.setup(
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Operating System :: OS Independent',
     ],
-    python_requires="!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires="!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
     packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     install_requires=[
         'rich'

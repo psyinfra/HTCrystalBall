76ae8263aa990f4ef1ef848cc827d17135f98421
Jona Fischer <jfischer@head1.htc.inm7.de>
2020-12-09 16:51:19 +0100

    clean up result output table (simplify values, unify for every type)

--- htcrystalball/display.py
+++ htcrystalball/display.py
@@ -93,11 +93,17 @@ def results(result: dict, verbose: bool, matlab: bool, n_cores: int, n_jobs: int
     Print out the preview result to the console using rich tables.
 
     Args:
+        wall_time: time per job, needed for total wall-time execution
+        n_jobs: number of requested jobs for wall-time execution
+        n_cores: number of requested cores for wall-time calculation
+        matlab: A bool telling whether matlab mode output is needed
         result: A dictionary of slot configurations including occupancy values
             for the requested job size.
         verbose: A value to extend the generated output.
     """
     console = Console()
+
+    # create table headers for verbose output
     if verbose:
         table = Table(show_header=True, header_style="bold cyan")
         table.add_column("Jobs", justify="right")
@@ -110,33 +116,29 @@ def results(result: dict, verbose: bool, matlab: bool, n_cores: int, n_jobs: int
     total_jobs = 0
     for slot in result['preview']:
         total_jobs += slot['sim_jobs']*slot['sim_slots']
-        node_jobs = slot['sim_jobs']*slot['sim_slots']
-        if slot["sim_slots"] != 1:
-            slot["sim_slots"] = "1.."+str(slot["sim_slots"])
-        if slot['fits'] == "YES":
-            color = 'green'
-            if verbose:
-                table.add_row(
-                    f"[{color}]{node_jobs}[/{color}]",
-                    f"[{color}]{slot['name']}[/{color}]",
-                    f"[{color}]{slot['sim_slots']}[/{color}]",
-                    f"[{color}]{slot['core_usage']} Cores[/{color}]",
-                    f"[{color}]{slot['ram_usage']}[/{color}]",
-                    f"[{color}]{slot['gpu_usage']}[/{color}]",
-                    f"[{color}]{slot['wall_time_on_idle']} min[/{color}]"
-                )
+
+        if int(slot['sim_jobs']) == 0:
+            node_jobs = 0
         else:
-            color = 'red'
-            if verbose or matlab:
-                table.add_row(
-                    f"[{color}]0[/{color}]",
-                    f"[{color}]{slot['name']}[/{color}]",
-                    f"[{color}]{slot['sim_slots']}[/{color}]",
-                    f"[{color}]{slot['core_usage']} Cores[/{color}]",
-                    f"[{color}]{slot['ram_usage']}[/{color}]",
-                    f"[{color}]{slot['gpu_usage']}[/{color}]",
-                    f"[{color}]{slot['wall_time_on_idle']} min[/{color}]"
-                )
+            node_jobs = slot['sim_jobs']*slot['sim_slots']
+        # create table row for verbose output
+        if verbose:
+            if slot["sim_slots"] != 1:
+                slot["sim_slots"] = "1.."+str(slot["sim_slots"])
+            if slot['fits'] == "YES":
+                color = 'green'
+            else:
+                color = 'red'
+
+            table.add_row(
+                f"[{color}]{node_jobs}[/{color}]",
+                f"[{color}]{slot['name']}[/{color}]",
+                f"[{color}]{slot['sim_slots']}[/{color}]",
+                f"[{color}]{slot['core_usage']} Cores[/{color}]",
+                f"[{color}]{slot['ram_usage']}[/{color}]",
+                f"[{color}]{slot['gpu_usage']}[/{color}]"
+            )
+    # write table and wall-time info to console
     if verbose:
         console.print("---------------------- PREVIEW ----------------------")
         console.print(table)
@@ -145,12 +147,15 @@ def results(result: dict, verbose: bool, matlab: bool, n_cores: int, n_jobs: int
         if wall_time > 0.0 and n_jobs > 0:
             time = int(max(n_jobs / total_jobs, 1)*wall_time / 60.0 + 1)
             core_hours = int(n_jobs * wall_time * n_cores / 60.0 + 1)
-            console.print("A total of "+str(core_hours)+" core-hour(s) will be used and will complete in about "+str(time)+" hour(s).")
+            console.print("A total of "+str(core_hours)+" core-hour(s) will be used and will complete in about " +
+                          str(time)+" hour(s).")
         else:
             console.print("No --jobs or --jobs-time specified. No duration estimate will be given.")
+    # write slot and wall-time info to console
     else:
         if total_jobs == 0:
-            console.print("Job size does not fit any compute slots. Use --verbose for details and a per-slot-config analysis.")
+            console.print("Job size does not fit any compute slots. "
+                          "Use --verbose for details and a per-slot-config analysis.")
         else:
             console.print(str(total_jobs)+" jobs of this size can run on this pool.")
             console.print("")
@@ -162,7 +167,8 @@ def results(result: dict, verbose: bool, matlab: bool, n_cores: int, n_jobs: int
             if wall_time > 0.0 and n_jobs > 0:
                 time = int(max(n_jobs / total_jobs, 1)*wall_time / 60.0 + 1)
                 core_hours = int(n_jobs * wall_time * n_cores / 60.0 + 1)
-                console.print("A total of "+str(core_hours)+" core-hour(s) will be used and will complete in about "+str(time)+" hour(s).")
+                console.print("A total of "+str(core_hours)+" core-hour(s) will be used and will complete in about " +
+                              str(time)+" hour(s).")
             else:
                 console.print("No --jobs or --jobs-time specified. No duration estimate will be given.")
             console.print("")

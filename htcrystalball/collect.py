abe79692e7f83bfd3c4f99601f5b6aae8f71ca56
Jona Fischer <jfischer@head1.htc.inm7.de>
2021-01-05 14:04:53 +0100

    add a list that stores the counters for each slot config

--- htcrystalball/collect.py
+++ htcrystalball/collect.py
@@ -22,14 +22,19 @@ def collect_slots(content: object) -> dict:
         if nodename not in unique_slots:
             unique_slots[nodename] = []
             unique_slots[nodename].append(slot_as_dict)
-            unique_slots[nodename][0]["SimSlots"] = 1
             sim_slot_values.append({'node': nodename, "slot": slot_as_dict, "sim_slots": 1})
         elif slot_as_dict not in unique_slots[nodename]:
-            slot_as_dict["SimSlots"] = 1
             unique_slots[nodename].append(slot_as_dict)
+            sim_slot_values.append({'node': nodename, "slot": slot_as_dict, "sim_slots": 1})
         elif slot_as_dict in unique_slots[nodename]:
-            for slot_number in range(len(unique_slots[nodename])):
-                if slot_as_dict == unique_slots[nodename][slot_number]:
-                    unique_slots[nodename][slot_number]["SimSlots"] += 1
+            for slot_number in range(len(sim_slot_values)):
+                if slot_as_dict == sim_slot_values[slot_number]["slot"] and nodename == sim_slot_values[slot_number]["node"]:
+                    sim_slot_values[slot_number]["sim_slots"] += 1
+
+    for elem in sim_slot_values:
+        for slot_number in range(len(unique_slots[elem["node"]])):
+            if elem["slot"] == unique_slots[elem["node"]][slot_number]:
+                unique_slots[elem["node"]][slot_number]["SimSlots"] = elem["sim_slots"]
+
 
     return unique_slots

058fa329fc7c729ad70a9ba5e34253fdb6cb7152
jonamfischer <j.fischer@fz-juelich.de>
2021-02-15 14:14:39 +0100

    add parser to tests and add testcases that use a submit file

--- tests/test_crystal_ball.py
+++ tests/test_crystal_ball.py
@@ -2,8 +2,10 @@
 
 import argparse
 import sys
+import os
 
 from pytest import raises as praises
+from testfixtures import TempDirectory
 sys.modules['htcondor'] = __import__('mock_htcondor')
 from htcondor import Collector as mocked_collector
 
@@ -120,6 +122,31 @@ def test_compare_request_available():
     assert utils.compare_requested_available(0, 8) == "green"
 
 
+def test_submit_parser():
+    """
+    Tests the .submit file parser.
+    :return:
+    """
+
+    with TempDirectory() as d:
+        d.write('test.submit', b'request_cpus = 1\n' + b'request_memory = 6000MB\n' + b'Queue 10')
+        assert utils.parse_submit_file(d.path+'/test.submit')["cpu"] == 1
+        assert utils.parse_submit_file(d.path+'/test.submit')["ram"] == "6000MB"
+        assert utils.parse_submit_file(d.path+'/test.submit')["jobs"] == 10
+
+    with TempDirectory() as d:
+        d.write('test.submit', b'request_cpus=1\n' + b'request_memory=6000MB\n' + b'Queue 10')
+        assert utils.parse_submit_file(d.path+'/test.submit')["cpu"] == 1
+        assert utils.parse_submit_file(d.path+'/test.submit')["ram"] == "6000MB"
+        assert utils.parse_submit_file(d.path+'/test.submit')["jobs"] == 10
+
+    with TempDirectory() as d:
+        d.write('test.submit', b'request_cpus=16\n' + b'request_memory=6GB\n' + b'Queue     10')
+        assert utils.parse_submit_file(d.path+'/test.submit')["cpu"] == 16
+        assert not utils.parse_submit_file(d.path+'/test.submit')["ram"] == "6000MB"
+        assert utils.parse_submit_file(d.path+'/test.submit')["jobs"] == 10
+
+
 def test_calc_manager():
     """
     Tests the method for preparing the slot checking.
@@ -129,68 +156,97 @@ def test_calc_manager():
 
     assert examine.prepare(
         cpu=1, gpu=0, ram="10GB", disk="0", jobs=1, job_duration="10m",
-        maxnodes=0, verbose=True, content=mocked_content
+        maxnodes=0, file="", verbose=True, content=mocked_content
     )
     assert not examine.prepare(
         cpu=0, gpu=1, ram="10GB", disk="0", jobs=1, job_duration="10m",
-        maxnodes=0, verbose=True, content=mocked_content
+        maxnodes=0, file="", verbose=True, content=mocked_content
     )
     assert not examine.prepare(
         cpu=1, gpu=0, ram="0", disk="", jobs=1, job_duration="", maxnodes=0,
-        verbose=True, content=mocked_content
+        verbose=True, file="", content=mocked_content
     )
     assert examine.prepare(
         cpu=1, gpu=1, ram="20GB", disk="", jobs=1, job_duration="10m",
-        maxnodes=0, verbose=True, content=mocked_content
+        maxnodes=0, file="", verbose=True, content=mocked_content
     )
     assert examine.prepare(
         cpu=1, gpu=0, ram="10GB", disk="10GB", jobs=1, job_duration="10m",
-        maxnodes=0, verbose=True, content=mocked_content
+        maxnodes=0, file="", verbose=True, content=mocked_content
     )
     assert examine.prepare(
         cpu=1, gpu=0, ram="10GB", disk="10GB", jobs=128, job_duration="15m",
-        maxnodes=0, verbose=True, content=mocked_content
+        maxnodes=0, file="", verbose=True, content=mocked_content
     )
     assert examine.prepare(
         cpu=1, gpu=0, ram="10GB", disk="", jobs=1, job_duration="10m",
-        maxnodes=1, verbose=True, content=mocked_content
+        maxnodes=1, file="", verbose=True, content=mocked_content
     )
     assert examine.prepare(
         cpu=8, gpu=0, ram="10GB", disk="", jobs=1, job_duration="10m",
-        maxnodes=0, verbose=True, content=mocked_content
+        maxnodes=0, file="", verbose=True, content=mocked_content
     )
     assert examine.prepare(
         cpu=8, gpu=0, ram="80GB", disk="", jobs=4, job_duration="1h",
-        maxnodes=0, verbose=True, content=mocked_content
+        maxnodes=0, file="", verbose=True, content=mocked_content
     )
     assert examine.prepare(
         cpu=2, gpu=0, ram="10GB", disk="", jobs=1, job_duration="10m",
-        maxnodes=3, verbose=True, content=mocked_content
+        maxnodes=3, file="", verbose=True, content=mocked_content
     )
     assert examine.prepare(
         cpu=1, gpu=0, ram="20GB", disk="", jobs=1, job_duration="10m",
-        maxnodes=2, verbose=True, content=mocked_content
+        maxnodes=2, file="", verbose=True, content=mocked_content
     )
     assert examine.prepare(
         cpu=2, gpu=0, ram="20GB", disk="", jobs=1, job_duration="",
-        maxnodes=2, verbose=True, content=mocked_content
+        maxnodes=2, file="", verbose=True, content=mocked_content
     )
     assert not examine.prepare(
         cpu=2, gpu=0, ram="20GB", disk="", jobs=2, job_duration="",
-        maxnodes=2, verbose=True, content=mocked_content
+        maxnodes=2, file="", verbose=True, content=mocked_content
     )
     assert not examine.prepare(
         cpu=2, gpu=0, ram="20GB", disk="", jobs=0, job_duration="10m",
-        maxnodes=2, verbose=True, content=mocked_content
+        maxnodes=2, file="", verbose=True, content=mocked_content
     )
     assert examine.prepare(
         cpu=2, gpu=0, ram="20GB", disk="", jobs=32, job_duration="10m",
-        maxnodes=1, verbose=True, content=mocked_content
+        maxnodes=1, file="", verbose=True, content=mocked_content
     )
     assert examine.prepare(
         cpu=2, gpu=5, ram="10GB", disk="", jobs=32, job_duration="10m",
-        maxnodes=1, verbose=True, content=mocked_content
+        maxnodes=1, file="", verbose=True, content=mocked_content
     )
+
+    # test with .submit files
+    with TempDirectory() as d:
+        d.write('test.submit', b'request_cpus = 1\n' + b'request_memory = 6000MB\n' + b'Queue 10')
+        assert examine.prepare(
+            cpu=0, gpu=0, ram="", disk="", jobs=1, job_duration="10m",
+            maxnodes=0, file=d.path+'/test.submit', verbose=True, content=mocked_content
+        )
+
+    with TempDirectory() as d:
+        d.write('test.submit', b'request_cpus = 1\n' + b'Queue 10')
+        assert not examine.prepare(
+            cpu=0, gpu=0, ram="", disk="", jobs=1, job_duration="10m",
+            maxnodes=0, file=d.path+'/test.submit', verbose=True, content=mocked_content
+        )
+
+    with TempDirectory() as d:
+        d.write('test.submit', b'request_cpus = 1\n' + b'Queue 10')
+        assert not examine.prepare(
+            cpu=0, gpu=0, ram="1G", disk="", jobs=1, job_duration="",
+            maxnodes=0, file=d.path+'/test.submit', verbose=True, content=mocked_content
+        )
+
+    with TempDirectory() as d:
+        d.write('test.submit', b'request_cpus = 1c\n' + b'request_memory = 6000MBc\n' + b'Queue 10c')
+        assert not examine.prepare(
+            cpu=0, gpu=0, ram="", disk="", jobs=1, job_duration="",
+            maxnodes=0, file=d.path+'/test.submit', verbose=True, content=mocked_content
+        )
 
 
 def test_slot_config():
